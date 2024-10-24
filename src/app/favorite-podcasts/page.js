bc6a8eeafcd9c5d6a2e91ca03c1e9366f18f2db9
MaxJones129 <maxjones129@gmail.com>
2024-10-23 19:27:55 -0500

    FAVORITE FUNCTIONALITY ADDED, also added useAuth throughout the page.

--- src/app/favorite-podcasts/page.js
+++ src/app/favorite-podcasts/page.js
@@ -7,13 +7,15 @@ import { Button } from 'react-bootstrap';
 import Link from 'next/link';
 import PodCard from '@/components/PodcastCard';
 import { getFavPods } from '@/api/podData';
+import { useAuth } from '../../utils/context/authContext';
 
 function FavoritePods() {
   const [pods, setPods] = useState([]);
 
+  const { user } = useAuth();
   // TODO: create a function that makes the API call to get all the books
   const getFavPodList = () => {
-    getFavPods(1).then(setPods);
+    getFavPods(user.id).then(setPods);
   };
 
   // TODO: make the call to the API to get all the books on component render
